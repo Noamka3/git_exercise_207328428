03c3a917b5410c0e1c36b21dd98d7c93857493db
Noamka3 <noamkadosh@gmail.com>
2025-03-17 17:37:01 +0200

    Adding additional printing to a function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ def add_numbers(a, b):
     return a + b
 
 if __name__ == "__main__":
+    print("This is a simple addition function")
     print(f"3 + 5 = {add_numbers(3, 5)}")
 def multiply_numbers(a, b):
     return a * b
